7a95d3a4d6e5a0d45632d8d8a8eeedb1ab036e85
Bulik László <laszlo.bulik@ksh.hu>
2017-02-23 12:36:35 +0100

    Added Hungarian translation.

---
+++ lang/date.format.hu.js
@@ -0,0 +1,13 @@
+/*!
+ * Hungarian translation for Date#format()
+ * Include after the main package.
+*/
+Date.format.translation.hu = {
+    monthNames: ['január', 'február', 'március', 'április', 'május', 'június', 'július', 'augusztus', 'szeptember', 'október', 'november', 'december'],
+    monthNamesShort: ['jan', 'febr', 'márc', 'ápr', 'máj', 'jún', 'júl', 'aug', 'szept', 'okt', 'nov', 'dec'],
+    dayNames: ['vasárnap', 'hétfő', 'kedd', 'szerda', 'csütörtök', 'péntek', 'szombat'],
+    dayNamesShort: ['V', 'H', 'K', 'Sze', 'Cs', 'P', 'Szo'],
+    ordinals: ['', '', '', ''],
+    am: 'de',
+    pm: 'du'
+};
